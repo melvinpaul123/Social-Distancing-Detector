9eabb484592f6951657e2c62372d1e2db10f3e8f
Melvin Paul Jacob <59479561+melvinpaul123@users.noreply.github.com>
2021-02-04 02:42:59 +0530

    Add files via upload

---
+++ social disatance checker.py
@@ -0,0 +1,91 @@
+import numpy as np
+import cv2
+from scipy.spatial import distance as dist
+import imutils
+import os
+
+MODEL_PATH = "yolo-coco"
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+USE_GPU = False
+MIN_DISTANCE = 50
+    
+def detect_people(frame, net, ln, personIdx=0):
+    (H, W) = frame.shape[:2]
+    results = []
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
+    boxes = []
+    centroids = []
+    confidences = []
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if classID == personIdx and confidence > MIN_CONF:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                centroids.append((centerX, centerY))
+                confidences.append(float(confidence))
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            r = (confidences[i], (x, y, x + w, y + h), centroids[i])
+            results.append(r)
+    return results
+
+labelsPath = os.path.sep.join([MODEL_PATH, "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+weightsPath = os.path.sep.join([MODEL_PATH, "yolov3.weights"])
+configPath = os.path.sep.join([MODEL_PATH, "yolov3.cfg"])
+
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+if USE_GPU:
+	print("[INFO] setting preferable backend and target to CUDA...")
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+print("[INFO] accessing video stream...")
+vs = cv2.VideoCapture("pedestrians.mp4")
+
+while True:
+    (grabbed, frame) = vs.read()
+    if not grabbed:
+        break
+    frame = imutils.resize(frame, width=700)
+    results = detect_people(frame, net, ln, personIdx=LABELS.index("person"))
+    violate = set()
+    if len(results) >= 2:
+        centroids = np.array([r[2] for r in results])
+        D = dist.cdist(centroids, centroids, metric="euclidean")
+        for i in range(0, D.shape[0]):
+            for j in range(i + 1, D.shape[1]):
+                if D[i, j] < MIN_DISTANCE:
+                    violate.add(i)
+                    violate.add(j)
+    for (i, (prob, bbox, centroid)) in enumerate(results):
+        (startX, startY, endX, endY) = bbox
+        (cX, cY) = centroid
+        color = (0, 255, 0)
+        if i in violate:
+            color = (0, 0, 255)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        cv2.circle(frame, (cX, cY), 5, color, 1)
+    text = "Social Distancing Violations: {}".format(len(violate))
+    cv2.putText(frame, text, (10, frame.shape[0] - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+    
+    cv2.imshow("Output", frame)
+    k = cv2.waitKey(10) & 0xff
+    if k == 27:
+        print("-> Ending Video Stream")
+        cv2.destroyAllWindows()
+        break  
